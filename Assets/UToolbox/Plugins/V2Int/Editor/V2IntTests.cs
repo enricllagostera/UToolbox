9d548d2cb4a97be6eb59a8688a6f581c3aa0a7da
Enric Llagostera <enricllagostera@gmail.com>
2017-06-27 13:36:58 -0300

    basic implementation

--- Assets/UToolbox/Plugins/V2Int/Editor/V2IntTests.cs
+++ Assets/UToolbox/Plugins/V2Int/Editor/V2IntTests.cs
@@ -3,7 +3,7 @@
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
-using UToolbox.SmartBagSystem;
+using UToolbox.V2IntSystem;
 using NUnit.Framework;
 
 namespace UToolbox.Tests
@@ -16,6 +16,69 @@ namespace UToolbox.Tests
             V2Int n = new V2Int(1, 1);
             Assert.AreEqual(1, n.x);
             Assert.AreEqual(1, n.y);
+        }
+
+        [Test]
+        public void V2Int_ToV2()
+        {
+            V2Int n = new V2Int(1, 1);
+            var v2 = n.ToV2();
+            Assert.AreEqual(1, v2.x);
+            Assert.AreEqual(1, v2.y);
+        }
+
+        [Test]
+        public void V2Int_FromV2()
+        {
+            var v2 = new Vector2(1, 1);
+            V2Int n = V2Int.FromV2(v2);
+            Assert.AreEqual(n.x, v2.x);
+            Assert.AreEqual(n.y, v2.y);
+        }
+
+        [Test]
+        public void V2Int_ClampSelf()
+        {
+            var v2 = new V2Int(-5, 5);
+            v2.ClampSelf(0, 4, 0, 4);
+            Assert.AreEqual(0, v2.x);
+            Assert.AreEqual(4, v2.y);
+            v2 = new V2Int(5, -5);
+            v2.ClampSelf(0, 4, 0, 4);
+            Assert.AreEqual(4, v2.x);
+            Assert.AreEqual(0, v2.y);
+        }
+
+        [Test]
+        public void V2Int_IsValid()
+        {
+            var v2 = new V2Int(-5, 5);
+            Assert.False(v2.IsValid(0, 2, 0, 2));
+            Assert.True(v2.IsValid(-10, 10, -10, 10));
+        }
+
+        [Test]
+        public void V2Int_Equals()
+        {
+            var a = new V2Int(-5, 5);
+            var b = new V2Int(-5, 5);
+            Assert.True(a == b);
+            b = new V2Int(5, 5);
+            Assert.True(a != b);
+        }
+
+        [Test]
+        public void V2Int_AtDirection()
+        {
+            var a = new V2Int(0, 0);
+            Assert.AreEqual(1, V2Int.AtDirection(a, EDirection.N).y);
+            Assert.AreEqual(1, V2Int.AtDirection(a, EDirection.E).x);
+            Assert.AreEqual(-1, V2Int.AtDirection(a, EDirection.W).x);
+            Assert.AreEqual(-1, V2Int.AtDirection(a, EDirection.S).y);
+            Assert.AreEqual(new V2Int(1, 1), V2Int.AtDirection(a, EDirection.NE));
+            Assert.AreEqual(new V2Int(1, -1), V2Int.AtDirection(a, EDirection.SE));
+            Assert.AreEqual(new V2Int(-1, -1), V2Int.AtDirection(a, EDirection.SW));
+            Assert.AreEqual(new V2Int(-1, 1), V2Int.AtDirection(a, EDirection.NW));
         }
     }
 }
--- Assets/UToolbox/Plugins/V2Int/V2Int.cs
+++ Assets/UToolbox/Plugins/V2Int/V2Int.cs
@@ -1,67 +1,123 @@
-﻿using UnityEngine;
+﻿/* Enric Llagostera <http://enric.llagostera.com.br> */
 
-[System.Serializable]
-public struct V2Int {
+using UnityEngine;
 
-	public int x, y;
-
-	public V2Int(int x, int y)
-	{
-		this.x = x;
-		this.y = y;
-	}
-
-	public Vector2 ToV2() {
-		return new Vector2(this.x, this.y);
-	}
-
-    public V2Int FromV2(Vector2 v2) {
-		return new V2Int((int)v2.x, (int)v2.y);
-	}
-
-    public V2Int Clamp(int minX, int maxX, int minY, int maxY){
-        V2Int nova = new V2Int(0,0);
-        if (x < minX) {
-            nova.x = minX;
-        }
-        if (x > maxX) {
-            nova.x = maxX;
-        }
-        if (y < minY) {
-            nova.y = minY;
-        }
-        if (y > maxY) {
-            nova.y = maxY;
-        }
-        return nova;
-    }
-
-    public bool Valido(int minX, int maxX, int minY, int maxY){
-        if (x < minX) {
-            return false;
-        }
-        if (x > maxX) {
-            return false;
-        }
-        if (y < minY) {
-            return false;
-        }
-        if (y > maxY) {
-            return false;
-        }
-        return true;
-    }
-
-    public override bool Equals(object obj) {
-        // Check for null values and compare run-time types.
-      if (obj == null || GetType() != obj.GetType()) 
-         return false;
-      V2Int p = (V2Int)obj;
-      return (x == p.x) && (y == p.y);
-    }
-
-    public override int GetHashCode() 
+namespace UToolbox.V2IntSystem
+{
+    public enum EDirection
     {
-        return x ^ y;
+        N,
+        NE,
+        E,
+        SE,
+        S,
+        SW,
+        W,
+        NW
+    }
+
+
+    [System.Serializable]
+    public struct V2Int
+    {
+
+        public int x, y;
+
+        public V2Int(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Vector2 ToV2()
+        {
+            return new Vector2(this.x, this.y);
+        }
+
+        public static V2Int FromV2(Vector2 v2)
+        {
+            return new V2Int((int)v2.x, (int)v2.y);
+        }
+
+        public void ClampSelf(int minX, int maxX, int minY, int maxY)
+        {
+            if (x < minX)
+            {
+                x = minX;
+            }
+            if (x > maxX)
+            {
+                x = maxX;
+            }
+            if (y < minY)
+            {
+                y = minY;
+            }
+            if (y > maxY)
+            {
+                y = maxY;
+            }
+        }
+
+        public bool IsValid(int minX, int maxX, int minY, int maxY)
+        {
+            if (x < minX)
+            {
+                return false;
+            }
+            if (x > maxX)
+            {
+                return false;
+            }
+            if (y < minY)
+            {
+                return false;
+            }
+            if (y > maxY)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+            V2Int p = (V2Int)obj;
+            return (x == p.x) && (y == p.y);
+        }
+
+        public override int GetHashCode()
+        {
+            return x ^ y;
+        }
+
+        public static bool operator ==(V2Int a, V2Int b)
+        {
+            return a.x == b.x && a.y == b.y;
+        }
+
+        public static bool operator !=(V2Int a, V2Int b)
+        {
+            return !(a == b);
+        }
+
+        public static V2Int AtDirection(V2Int position, EDirection direction)
+        {
+            var newPosition = position;
+            switch (direction)
+            {
+                case EDirection.N: newPosition.y++; break;
+                case EDirection.NE: newPosition.y++; newPosition.x++; break;
+                case EDirection.E: newPosition.x++; break;
+                case EDirection.SE: newPosition.y--; newPosition.x++; break;
+                case EDirection.S: newPosition.y--; break;
+                case EDirection.SW: newPosition.y--; newPosition.x--; break;
+                case EDirection.W: newPosition.x--; break;
+                case EDirection.NW: newPosition.y++; newPosition.x--; break;
+            }
+            return newPosition;
+        }
     }
 }
